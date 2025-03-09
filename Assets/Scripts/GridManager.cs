dde02157647616644642d61e991079033e96d45c
Midnigh1 <issarawutjohnny@gmail.com>
2025-03-09 21:34:09 +0100

    Scripts changes

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GridManager : MonoBehaviour
+{
+    public GameObject prefab;
+    public int width = 30, height = 30;
+    public float spacing = 1.5f, waveSpeed = 2.0f, waveAmplitude = 1.5f;
+    public Gradient colorGradient;
+
+    private Transform[,] grid;
+    private float[,] waveOffsets;
+
+    public Transform[,] Grid => grid; // Public getter for ShockwaveManager
+
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    void Update()
+    {
+        ApplyWaveEffect();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Transform[width, height];
+        waveOffsets = new float[width, height];
+
+        Vector3 basePosition = transform.position;
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Vector3 position = basePosition + new Vector3(x * spacing, 0, z * spacing);
+                GameObject obj = Instantiate(prefab, position, Quaternion.identity, transform);
+                grid[x, z] = obj.transform;
+                waveOffsets[x, z] = Random.Range(0f, Mathf.PI * 2);
+
+                Renderer rend = obj.GetComponent<Renderer>();
+                if (rend != null) rend.material = new Material(rend.material);
+
+                Destroy(obj.GetComponent<BoxCollider>()); // Remove BoxCollider if present
+            }
+        }
+        AddFloorCollider();
+    }
+
+    void AddFloorCollider()
+    {
+        BoxCollider box = gameObject.AddComponent<BoxCollider>();
+        box.center = new Vector3((width - 1) * spacing / 2, 3.5f, (height - 1) * spacing / 2);
+        box.size = new Vector3(width * spacing, 1f, height * spacing);
+    }
+
+    void ApplyWaveEffect()
+    {
+        float time = Time.time * waveSpeed;
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Transform obj = grid[x, z];
+                float waveHeight = Mathf.Sin(time + waveOffsets[x, z]) * waveAmplitude;
+                obj.position = new Vector3(obj.position.x, waveHeight, obj.position.z);
+
+                float normalizedHeight = Mathf.InverseLerp(-waveAmplitude, waveAmplitude, waveHeight);
+                obj.GetComponent<Renderer>().material.color = colorGradient.Evaluate(normalizedHeight);
+            }
+        }
+    }
+}
+
--- Assets/Scripts/ShockwaveAbility.cs
+++ Assets/Scripts/ShockwaveAbility.cs
@@ -2,80 +2,44 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class RandomWaveGrid : MonoBehaviour
+public class ShockwaveAbility : MonoBehaviour
 {
-    public GameObject prefab;
-    public int width = 30, height = 30;
-    public float spacing = 1.5f, waveSpeed = 2.0f, waveAmplitude = 1.5f;
     public float shockwaveSpeed = 8.0f, shockwaveMaxRadius = 10.0f, heightMultiplier = 3.0f;
     public float shockwaveDuration = 0.5f;
-    public Gradient colorGradient, shockwaveGradient;
+    public Gradient shockwaveGradient;
 
-    private Transform[,] grid;
-    private float[,] waveOffsets;
+    private List<GridManager> gridManagers = new List<GridManager>(); // Track all GridManagers
     private Dictionary<Transform, Vector3> originalScales = new Dictionary<Transform, Vector3>();
     private List<Shockwave> activeShockwaves = new List<Shockwave>();
 
-    void Start()
+    IEnumerator Start()
     {
-        CreateGrid();
+        yield return new WaitForEndOfFrame(); // Wait for GridManager to initialize
+
+        gridManagers.AddRange(FindObjectsByType<GridManager>(FindObjectsSortMode.None));
+
+        if (gridManagers.Count == 0)
+        {
+            Debug.LogError("No GridManager instances found! Make sure they exist in the scene.");
+            yield break;
+        }
+
+        CacheOriginalScales();
     }
 
     void Update()
     {
-        ApplyWaveEffect();
-        UpdateShockwaves();
         if (Input.GetMouseButtonDown(0)) DetectShockwaveOrigin();
+        UpdateShockwaves();
     }
 
-    void CreateGrid()
+    void CacheOriginalScales()
     {
-        grid = new Transform[width, height];
-        waveOffsets = new float[width, height];
-
-        Vector3 basePosition = transform.position;
-        for (int x = 0; x < width; x++)
+        foreach (GridManager gridManager in gridManagers)
         {
-            for (int z = 0; z < height; z++)
+            foreach (Transform obj in gridManager.Grid)
             {
-                Vector3 position = basePosition + new Vector3(x * spacing, 0, z * spacing);
-                GameObject obj = Instantiate(prefab, position, Quaternion.identity, transform);
-                Transform objTransform = obj.transform;
-                grid[x, z] = objTransform;
-                waveOffsets[x, z] = Random.Range(0f, Mathf.PI * 2);
-
-                Renderer rend = obj.GetComponent<Renderer>();
-                if (rend != null) rend.material = new Material(rend.material);
-
-                // Remove BoxCollider if present
-                Destroy(obj.GetComponent<BoxCollider>());
-
-                originalScales[objTransform] = objTransform.localScale;
-            }
-        }
-        AddFloorCollider();
-    }
-
-    void AddFloorCollider()
-    {
-        BoxCollider box = gameObject.AddComponent<BoxCollider>();
-        box.center = new Vector3((width - 1) * spacing / 2, 4f-0.5f, (height - 1) * spacing / 2);
-        box.size = new Vector3(width * spacing, 1f, height * spacing);
-    }
-
-    void ApplyWaveEffect()
-    {
-        float time = Time.time * waveSpeed;
-        for (int x = 0; x < width; x++)
-        {
-            for (int z = 0; z < height; z++)
-            {
-                Transform obj = grid[x, z];
-                float waveHeight = Mathf.Sin(time + waveOffsets[x, z]) * waveAmplitude;
-                obj.position = new Vector3(obj.position.x, waveHeight, obj.position.z);
-
-                float normalizedHeight = Mathf.InverseLerp(-waveAmplitude, waveAmplitude, waveHeight);
-                obj.GetComponent<Renderer>().material.color = colorGradient.Evaluate(normalizedHeight);
+                originalScales[obj] = obj.localScale;
             }
         }
     }
@@ -104,12 +68,15 @@ public class RandomWaveGrid : MonoBehaviour
                 continue;
             }
 
-            foreach (Transform obj in grid)
+            foreach (GridManager gridManager in gridManagers)
             {
-                float distanceSqr = (obj.position - wave.origin).sqrMagnitude;
-                if (distanceSqr < radiusSqr && distanceSqr > (radiusSqr - 2.25f)) // Using squared values
+                foreach (Transform obj in gridManager.Grid)
                 {
-                    StartCoroutine(ShockwaveEffect(obj, Mathf.Sqrt(distanceSqr) / shockwaveMaxRadius));
+                    float distanceSqr = (obj.position - wave.origin).sqrMagnitude;
+                    if (distanceSqr < radiusSqr && distanceSqr > (radiusSqr - 2.25f)) // Using squared values
+                    {
+                        StartCoroutine(ShockwaveEffect(obj, Mathf.Sqrt(distanceSqr) / shockwaveMaxRadius));
+                    }
                 }
             }
         }
@@ -117,7 +84,13 @@ public class RandomWaveGrid : MonoBehaviour
 
     IEnumerator ShockwaveEffect(Transform obj, float normalizedDistance)
     {
-        Vector3 originalScale = originalScales[obj];
+        if (!originalScales.ContainsKey(obj))
+        {
+            Debug.LogWarning($"Missing key in originalScales dictionary: {obj.name}");
+            yield break; // Exit the coroutine if key is missing
+        }
+
+        Vector3 originalScale = originalScales[obj]; // Safe access now
         Renderer rend = obj.GetComponent<Renderer>();
         Color originalColor = rend.material.color;
         Color waveColor = shockwaveGradient.Evaluate(normalizedDistance);
@@ -153,4 +126,3 @@ public class RandomWaveGrid : MonoBehaviour
         public Shockwave(Vector3 origin, float startTime) { this.origin = origin; this.startTime = startTime; }
     }
 }
-
